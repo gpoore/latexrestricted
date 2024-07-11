0c4dae80ea06b5d16bc6a599146b84ccc85a7226
Geoffrey Poore <gpoore@gmail.com>
2024-07-11 14:43:31 -0500

    restricted_run(): minor improvements to docs, comments, and variable names; updated error classes

--- latexrestricted/_restricted_subprocess.py
+++ latexrestricted/_restricted_subprocess.py
@@ -13,15 +13,14 @@ from __future__ import annotations
 import platform
 import shutil
 import subprocess
-from ..err import SubprocessUnapprovedExecutableError, SubprocessExecutableNotFoundError, SubprocessExecutablePathSecurityError
+from .err import UnapprovedExecutableError, ExecutableNotFoundError, ExecutablePathSecurityError
 from ._anypath import AnyPath
 from ._latex_config import latex_config
-from ._restricted_pathlib import RestrictedPath
 
 
 
 
-_approved_executables = set([
+_always_approved_executables = set([
     'kpsewhich',
     'initexmf',
 ])
@@ -40,10 +39,10 @@ def restricted_run(args: list[str], allow_restricted_executables: bool = False) 
     '''
     Run a command securely, consistent with TeX restricted shell escape.
 
-     *  By default, the executable must be in the list of approved executables
-        in this file.  If `allow_restricted_executables == True`, then the
-        executable must be in the list of allowed restricted shell escape
-        commands as returned by `kpsewhich` or `initexmf`.
+     *  By default, the executable must be in the list of always approved
+        executables in this file.  If `allow_restricted_executables == True`,
+        then the executable must be in the list of allowed restricted shell
+        escape commands as returned by `kpsewhich` or `initexmf`.
 
      *  The executable must be in the same directory as `kpsewhich` or
         `initexmf`, as previously located during LaTeX config evaluation, or
@@ -67,9 +66,9 @@ def restricted_run(args: list[str], allow_restricted_executables: bool = False) 
         raise TypeError
 
     executable = args[0]
-    if executable not in _approved_executables:
+    if executable not in _always_approved_executables:
         if not allow_restricted_executables or executable not in latex_config.restricted_shell_escape_commands:
-            raise SubprocessUnapprovedExecutableError(f'Executable "{executable}" is not in the approved list')
+            raise UnapprovedExecutableError(f'Executable "{executable}" is not in the approved list')
 
     if latex_config.texlive_bin:
         which_executable = shutil.which(executable, path=latex_config.texlive_bin)
@@ -80,31 +79,29 @@ def restricted_run(args: list[str], allow_restricted_executables: bool = False) 
     if not which_executable:
         which_executable = shutil.which(executable)
     if not which_executable:
-        raise SubprocessExecutableNotFoundError(f'Executable "{executable}" was not found')
+        raise ExecutableNotFoundError(f'Executable "{executable}" was not found')
 
-    which_executable_path = RestrictedPath(which_executable)
+    which_executable_path = AnyPath(which_executable)
     which_executable_resolved = which_executable_path.resolve()
     if platform.system() == 'Windows' and not which_executable_resolved.name.lower().endswith('.exe'):
-        raise SubprocessUnapprovedExecutableError(
+        raise UnapprovedExecutableError(
             f'Executable "{executable}" resolved to "{which_executable_path.as_posix()}", but *.exe is required'
         )
     if any(which_executable_path.name.lower().endswith(ext) for ext in ('.bat', '.cmd')):
         # This should be redundant
-        raise SubprocessUnapprovedExecutableError(f'Executable "{executable}" not allowed (no *.bat or *.cmd)')
+        raise UnapprovedExecutableError(f'Executable "{executable}" not allowed (no *.bat or *.cmd)')
 
     if any(e.is_relative_to(p) or p.is_relative_to(e)
            for e in set([which_executable_path.parent, which_executable_resolved.parent])
            for p in _prohibited_path_roots):
-        raise SubprocessExecutablePathSecurityError(
+        raise ExecutablePathSecurityError(
             f'Executable "{executable}" is located under the current directory, $TEXMFOUTPUT, '
             'or $TEXMF_OUTPUT_DIRECTORY, or one of these locations is under the same directory '
             'as the executable'
         )
 
-    # Use resolved executable path for subprocess.  Using just the executable
-    # name should give the same results, but this guards against any mismatch
-    # between how Python resolves the executable name with no path versus how
-    # the subprocess would resolve it.
+    # Use resolved executable path for subprocess to guarantee that the
+    # correct executable is invoked.
     resolved_args = [which_executable_path.as_posix()] + args[1:]
     proc = subprocess.run(resolved_args, shell=False, capture_output=True)
     return proc
